d545b0a85ec1cb51204dee0c0107dcf512e21134
joerex1418 <joe.rechenmacher@gmail.com>
2022-11-30 03:59:36 -0600

    Update __init__.py

--- bacondates/__init__.py
+++ bacondates/__init__.py
@@ -1,12 +1,12 @@
 """
-#BaconDates
+# BaconDates
 
 An extremely small conveniece library for dealing with date objects in Python
 
 Author: Joe Rechenmacheer
+E-Mail: joe.rechenmacher@gmail.com
 GitHub: https://github.com/joerex1418/bacondates
 License: MIT
-
 """
 
 from .funcs import add_months
