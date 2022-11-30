bc8597b418048b6856224900dd0be8df381ae4fa
Joe <67286232+joerex1418@users.noreply.github.com>
2022-11-30 03:53:25 -0600

    Update __init__.py

--- bacondates/__init__.py
+++ bacondates/__init__.py
@@ -1,3 +1,14 @@
+"""
+#BaconDates
+
+An extremely small conveniece library for dealing with date objects in Python
+
+Author: Joe Rechenmacheer
+GitHub: https://github.com/joerex1418
+License: MIT
+
+"""
+
 from .funcs import add_months
 from .funcs import is_leap_year
 
